3843985bc3701b87d6f518b14cac4c040127fd36
perniteo <perniteo1@gmail.com>
2023-11-20 18:06:06 +0900

    .

---
+++ java-basic/app/src/main/java/C.java
@@ -0,0 +1,9 @@
+class C1 {
+  
+}
+class C2 {
+  
+}
+class C3 {
+  
+}
---
+++ java-basic/app/src/main/java/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+  public static void main(String args[]) {
+    System.out.println(100);
+    System.out.println('가');
+    System.out.println(200);
+    System.out.println(false);
+
+  }
+}
