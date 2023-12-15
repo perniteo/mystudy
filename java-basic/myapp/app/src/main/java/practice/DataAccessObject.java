2901ed61253b67f64cfae54e52244e2bd3fdc774
perniteo <perniteo1@gmail.com>
2023-12-15 17:56:28 +0900

    .

---
+++ java-basic/myapp/app/src/main/java/practice/DataAccessObject.java
@@ -0,0 +1,12 @@
+package practice;
+
+public interface DataAccessObject {
+
+  public void select();
+
+  public void insert();
+
+  public void update();
+
+  public void delete();
+}
--- java-basic/myapp/app/src/main/java/practice/Main.java
+++ java-basic/myapp/app/src/main/java/practice/Main.java
@@ -1,31 +1,63 @@
 package practice;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int cookingTime = Integer.parseInt(br.readLine());
-    StringBuilder str = new StringBuilder();
-    while (st.hasMoreTokens()) {
-      str.append(st.nextToken());
-    }
-    int now = Integer.parseInt(str.toString());
-    int hour;
-    int minute;
-    if (cookingTime >= 60) {
-      hour = cookingTime / 60;
-      minute = cookingTime % 60;
-    } else {
-      hour = 0;
-      minute = cookingTime;
-    }
-    now += hour * 100 + minute;
-    System.out.println(now);
+//    AutoCloseable
+    System.in.close();
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    StringTokenizer st = new StringTokenizer(br.readLine());
+//    int cookingTime = Integer.parseInt(br.readLine());
+//    StringBuilder str = new StringBuilder();
+//    while (st.hasMoreTokens()) {
+//      str.append(st.nextToken());
+//    }
+//    int now = Integer.parseInt(str.toString());
+//    int hour;
+//    int minute;
+//    if (cookingTime >= 60) {
+//      hour = cookingTime / 60;
+//      minute = cookingTime % 60;
+//    } else {
+//      hour = 0;
+//      minute = cookingTime;
+//    }
+//    now += hour * 100 + minute;
+//    System.out.println(now);
+//    class Chatting {
+//
+//      void startChat(String chatId) {
+//        String nickName = null;
+//        nickName = chatId;
+//
+//        Chat chat = new Chat() {
+//          @Override
+//          public void start() {
+//            while (true) {
+//              String inputData = "안녕하세요";
+//              String message = "[" + nickName + "]" + inputData;
+//              sendMessage(message);
+//            }
+//          }
+//
+//        };
+//        chat.start();
+//      }
+//
+//      class Chat {
+//
+//        void start() {
+//        }
+//
+//        ;
+//
+//        void sendMessage(String message) {
+//        }
+//
+//        ;
+//      }
+//    }
   }
 }
